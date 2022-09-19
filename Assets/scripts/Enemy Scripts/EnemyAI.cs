d2950d06432fdd7847d066602c0f72b8d88b5324
Aidan <datbluemushroom@gmail.com>
2022-09-18 22:10:57 -0400

    🚀 Basic Enemy AI

--- Assets/scripts/Enemy Scripts/EnemyAI.cs
+++ Assets/scripts/Enemy Scripts/EnemyAI.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    // determines the speed of the enemy
+    public float speed;
+    public float attackRadius;
+
+    public bool shouldRotate;
+
+    private Transform target;
+    private Rigidbody2D rb;
+    private Animator anim;
+    private Vector2 movement;
+    public Vector3 dir;
+
+    private bool isInAttackRange;
+
     void Start()
     {
-        
+        rb = transform.GetComponent<Rigidbody2D>();
+        target = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        var step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
     }
 }
